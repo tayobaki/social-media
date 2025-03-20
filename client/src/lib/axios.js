4322e4b2b746d4f57bcdc6bc4a61b6002b783885
Bakii <tayolowosale@gmail.com>
2025-03-20 11:22:53 +0100

    update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api" // Local backend
+    : "https://social-media-green-five.vercel.app/api"; // Deployed backend
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
   withCredentials: true,
 });
 
